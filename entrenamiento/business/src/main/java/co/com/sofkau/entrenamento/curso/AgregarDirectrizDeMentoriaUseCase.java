ab4c090012b4bf70a479b31b06652694322d84db
pablo-davila1 <62685470+pablo-davila1@users.noreply.github.com>
2022-05-16 22:55:39 -0500

    Pruebas
    Se agregan las dependencias de mockito y junit, para proceder con las pruebas

--- entrenamiento/business/src/main/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizDeMentoriaUseCase.java
+++ entrenamiento/business/src/main/java/co/com/sofkau/entrenamento/curso/AgregarDirectrizDeMentoriaUseCase.java
@@ -4,7 +4,20 @@ package co.com.sofkau.entrenamento.curso;
 import co.com.sofka.business.generic.UseCase;
 import co.com.sofka.business.support.RequestCommand;
 import co.com.sofka.business.support.ResponseEvents;
+import co.com.sofkau.entrenamiento.curso.Curso;
+import co.com.sofkau.entrenamiento.curso.Mentoria;
+import co.com.sofkau.entrenamiento.curso.commands.AgregarDirectrizDeMentoria;
+import co.com.sofkau.entrenamiento.curso.values.Fecha;
+import co.com.sofkau.entrenamiento.curso.values.MentoriaId;
 
-public class AgregarDirectrizDeMentoriaUseCase extends UseCase<RequestCommand<>, ResponseEvents>  {
+public abstract class AgregarDirectrizDeMentoriaUseCase extends UseCase<RequestCommand<AgregarDirectrizDeMentoria>, ResponseEvents>  {
+    @Override
+    public void executeUseCase(RequestCommand<AgregarDirectrizDeMentoria> agregarDirectrizDeMentoriaRequestCommand){
+        var command = agregarDirectrizDeMentoriaRequestCommand.getCommand();
+        //var directrizdementoria = new Mentoria(command.getEntityId(), command.getNombre(), command.getFecha());
+        var directrizdementoria = new AgregarDirectrizDeMentoria(command.getEntityId(),command.getNombre(),command.getFecha());
+        //emit().onResponse(new ResponseEvents(directrizdementoria.getUncommittedChanges()));
 
+
+    }
 }
